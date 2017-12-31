7f6a47ed06964a9095d5a5bb2f93767641082737
kenpb <kpalmab@icloud.com>
2017-12-31 17:13:59 -0600

    Added the electron/react related code :shipit:

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,3 +1,15 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
 import './sass/main.sass'
 
-alert('success')
+class App extends Component {
+  render() {
+    return <h1>Hello From React!</h1>
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+)
