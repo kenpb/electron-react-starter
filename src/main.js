eb87aa27a060b3c6a5a80a9260c6de16db7aad6e
kenpb <kpalmab@icloud.com>
2017-12-31 04:51:51 -0600

    Added sass support

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
-alert(1)
+import './sass/main.sass'
+
+alert('success')
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,26 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.sass$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' }, 
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ],
+            },
+        ]
     }
+
 }
 
